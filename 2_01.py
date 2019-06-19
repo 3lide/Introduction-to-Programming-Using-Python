00c52daa4b91e5a01eb2d2560c04fc402e3f4d09
Heng Wang <wh9697yyk@163.com>
2019-06-20 01:20:39 +0800

    Delete 2_01.py

--- 2_01.py
+++
@@ -1,5 +0,0 @@
-degreeInCelsius = eval(input('Enter a degree in Celsius: '))
-
-fahrenheit = degreeInCelsius * (9 / 5)  + 32
-
-print("{} Celsius is {:.1f} Fahrenheit.".format(degreeInCelsius, fahrenheit))
