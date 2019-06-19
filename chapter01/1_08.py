facf03828f99200ceae4a260a545699eed542c3f
Heng Wang <wh9697yyk@163.com>
2019-06-19 20:46:49 +0800

    Update 1_08.py

--- chapter01/1_08.py
+++ chapter01/1_08.py
@@ -1,1 +1,6 @@
 
+import math
+radius = eval(input("Enter radius of circle: "))
+
+print("Circle's area is", format(math.pi * radius * radius, ".2f"))
+print("Circle's perimeter is", format(math.pi * 2 * radius, ".2f"))
