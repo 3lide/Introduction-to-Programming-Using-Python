0a14caba560cff51500d81f9c9e9f77fbd8f52a1
Heng Wang <wh9697yyk@163.com>
2019-06-19 21:48:28 +0800

    Add files via upload

---
+++ chapter01/1_13.py
@@ -0,0 +1,16 @@
+import turtle
+
+#绘制十字
+turtle.setup(1000, 1000)
+turtle.penup()
+turtle.pensize(5)
+turtle.goto(-300,0)
+turtle.pendown()
+turtle.forward(600)
+turtle.penup()
+turtle.goto(0, 300)
+turtle.setheading(-90)
+turtle.pendown()
+turtle.forward(600)
+turtle.turtlesize(3)
+turtle.done()
