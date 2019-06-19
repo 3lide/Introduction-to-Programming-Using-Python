f65de98babd2d327aa5440d215214fbf5c2b3524
Heng Wang <wh9697yyk@163.com>
2019-06-19 23:50:59 +0800

    Add files via upload

---
+++ 1_15.py
@@ -0,0 +1,17 @@
+import turtle
+
+#绘制两个三角形
+turtle.setup(1000, 1000)
+turtle.pensize(5)
+turtle.setheading(-60)
+turtle.forward(400)
+turtle.setheading(180)
+turtle.forward(400)
+turtle.right(120)
+turtle.forward(800)
+turtle.left(120)
+turtle.forward(400)
+turtle.left(120)
+turtle.forward(400)
+turtle.turtlesize(3)
+turtle.done()
