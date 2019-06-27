26bb99cc56c5a55c42f5b7ad2b3a1e4d4eb3ceb4
Heng Wang <wh9697yyk@163.com>
2019-06-27 12:22:35 +0800

    Update 4_01.py

--- chapter04/4_01.py
+++ chapter04/4_01.py
@@ -1,1 +1,15 @@
+import math
 
+a, b, c = eval(input("Enter a, b, c: "))
+
+delta = b**2 - 4 * a * c
+
+if delta > 0:
+    r1 = (-b + math.sqrt(delta)) / (2 * a)
+    r2 = (-b - math.sqrt(delta)) / (2 * a)
+    print("The roots are {:.5f} and {:.5f}".format(r1, r2))
+elif delta == 0:
+    r = -b / (2 * a)
+    print("The root is {:.5f}".format(r))
+else:
+    print("The equation has no real roots")
